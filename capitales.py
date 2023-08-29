bbd0b8959eaac2ef06c2d59877a0f845d402516c
jimy mora <jimymora@hotmail.com>
2023-08-28 22:27:01 -0500

    polimorfismo practicando

---
+++ capitales.py
@@ -0,0 +1,19 @@
+class Ciudades1:
+    def capitales( self):
+        print("Cali es la capital del Valle")
+
+
+class Ciudades2:
+    def capitales(self):
+          print("Medellin es la capital de Antioquia")
+
+class Ciudades3:
+     def capitales(self):
+          print("Ibague es la capital del Tolima")
+
+
+ciudad1 = Ciudades2()
+ciudad1.capitales()
+
+                
+
---
+++ capitales_return.py
@@ -0,0 +1,24 @@
+class Ciudades:
+    def capitales(self):
+        return "Barranquilla es la capital del Atlantico"
+    
+
+class Ciudades2:
+    def capitales(self):
+        return "Valledupar es la capital del Cesar"
+    
+
+class Ciudades3:
+    def capitales(self):
+        return("Cartagena es la capital de Bolivar")
+    
+
+
+quilla = Ciudades()
+print(quilla.capitales())
+
+quilla = Ciudades2()
+print(quilla.capitales())
+
+quilla = Ciudades3()
+print(quilla.capitales())
